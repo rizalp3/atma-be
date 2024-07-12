74a804cd813fd86cdee801ecde321a32fe35986f
rizalp3 <rizalp3@gmail.com>
2024-07-12 13:43:04 +0700

    Update test result endpoint

--- src/api/test-result/controllers/test-result.js
+++ src/api/test-result/controllers/test-result.js
@@ -1,9 +1,40 @@
-'use strict';
+// @ts-nocheck
+"use strict";
 
 /**
  * test-result controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::test-result.test-result');
+module.exports = createCoreController("api::test-result.test-result", () => ({
+  async find(ctx) {
+    const userId = ctx?.state?.user?.id;
+
+    if (userId) {
+      ctx.query = {
+        ...ctx.query,
+        filters: { user: { $eq: userId } },
+      };
+    }
+
+    const result = await super.find(ctx);
+
+    return result;
+  },
+
+  async create(ctx) {
+    const userId = ctx?.state?.user?.id;
+
+    if (userId) {
+      ctx.request.body.data = {
+        ...ctx.request.body.data,
+        user: userId,
+      };
+    }
+
+    const result = await super.create(ctx);
+
+    return result;
+  },
+}));
